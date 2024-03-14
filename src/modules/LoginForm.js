b7fcad2ac43a9d5b2e10b5e30ca7caed89533c74
wolfson-1 <harrywolverson@googlemail.com>
2024-03-14 17:34:58 +0700

    Initial setup of login form submission handling & firebase auth from backend.

--- src/modules/LoginForm.js
+++ src/modules/LoginForm.js
@@ -1,28 +1,50 @@
-import React, { useEffect } from 'react'
-import {db} from '../firebase/config';
-import useFetchDoc from '../hooks/useFetchDoc';
+import React, {useState } from 'react'
+import {auth} from '../firebase/config';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 export default function LoginForm() {
 
-  //   firebase data test fetch 
-  // const testData = useFetchDoc(db,['test','eB6OR2AIVvIZlrAUqPOL']);
+  //state for login user values & error
+  const [email,setEmail] = useState(null);
+  const [password,setPassword] = useState(null);
+  const [loginError,setLoginError] = useState(null);
 
-  //submit form function
+  //submit user data function
   const submitForm = (event) => {
     event.preventDefault();
-    };
+
+    if(email === null || password === null) {
+      setLoginError('Please enter both email & password to login.');
+      return;
+    }
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      console.log(error.code);
+      console.log(error.message);
+      setLoginError("email or password incorrect, please try logging in again.");
+      setEmail(null);
+      setPassword(null);
+    })
+};
 
   return (
     <form>
         <label>
             Email    
-            <input type='text' id='email' name='email'></input>
+            <input type='email' id='email' name='email' placeholder='example@gmail.com' value={email} onClick={() => {setLoginError(null)}} onChange={(e)=>{setEmail(e.target.value)}}></input>
         </label>
         <label>
             Password
-            <input type='password' id='password' name='password'></input>
+            <input type='password' id='password' name='password' placeholder='enter password' value={password} onClick={() => {setLoginError(null)}} onChange={(e)=>{setPassword(e.target.value)}}></input>
         </label>
         <input type='submit' value='login' onClick={submitForm}></input>
+        {loginError && <p>{loginError}</p>}
     </form>
   )
 };
